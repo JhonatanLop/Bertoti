f9ecc284551ed46e2b27fe6421470b964fb4a195
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-03-18 22:47:12 -0300

    adicionando melhorias à classe Carregador

--- Classes/Arma.java
+++ Classes/Arma.java
@@ -1,15 +1,37 @@
 package Classes;
+import java.util.Scanner;
 
 public class Arma {
+    Scanner leia = new Scanner(System.in);
+
     String tipo;
     String marca;
+    String atirar;
     double peso;
     double alcance;
     double tamanho;
     Carregador carregador;
 
     public Integer atirar(int qtd_balas) {
-        
+        System.out.println("Atirar? s/n");
+        atirar = leia.next();
+        if (atirar == "s") {
+            if (qtd_balas != 0) {
+                qtd_balas --;
+                System.out.println("POW!");
+            }
+            else{
+                String resp;
+                System.out.println("A munição acabou.");
+                System.out.println("deseja recarregar? s/n");
+                resp = leia.next();
+                if (resp == "s") {
+                    
+                }
+            }
+
+
+        }
         return qtd_balas;
     }
 }
--- Classes/Carregador.java
+++ Classes/Carregador.java
@@ -1,54 +1,64 @@
 package Classes;
+
 import java.util.Scanner;
 
 public class Carregador {
     static Scanner leia = new Scanner(System.in);
     static int resp;
-    
+
     String corTransportador;
     String tipoAlinhamento;
     int capacidade;
     int qtdMunicao;
     Municao tipoMunicao;
 
-    public Integer carregar(int qtdMunicao, int capacidade) {
-        System.out.println("vc tem " + qtdMunicao + " munições");
-        System.out.println("Deseja encher o carregaor?");
+    public static Integer carregar(int qtdMunicao, int capacidade) {
+        System.out.println("\nvc tem " + qtdMunicao + " munições");
+        System.out.println("Deseja encher o carregador?");
         System.out.println("1 - sim");
         System.out.println("2 - não");
         resp = leia.nextInt();
         if (resp == 1) {
-            System.out.println("carregando...");
+            System.out.println("\ncarregando...");
             qtdMunicao = capacidade;
         }
-        System.out.println("Seu carregador agora tem "+ qtdMunicao + " munições");
+        System.out.println("Seu carregador agora tem " + qtdMunicao + " munições");
         return qtdMunicao;
     }
 
-    public Integer descarregar(int qtdMunicao) {
-        System.out.println("Você tem " + qtdMunicao + " munições");
+    public static Integer descarregar(int qtdMunicao) {
+        System.out.println("\nVocê tem " + qtdMunicao + " munições");
         System.out.println("Deseja esvaziar o carregador?");
         System.out.println("1 - sim");
         System.out.println("2 - não");
         resp = leia.nextInt();
         if (resp == 1) {
-            System.out.println("Descarregando...");
-            qtdMunicao-= qtdMunicao;
+            System.out.println("\n Descarregando");
+            while (qtdMunicao != 0) {
+                System.out.println("\n*puxa o ferrolho*\n *bala cai*");
+                qtdMunicao --;
+            }
         }
-        System.out.println("Seu carregador está zerado!");
+        System.out.println("\nSeu carregador está zerado!");
         return qtdMunicao;
     }
 
-    public Carregador cadastrarCarregador() {
+    public static Carregador cadastrarCarregador() {
         Carregador magazineCarregador = new Carregador();
-
-        magazineCarregador.corTransportador = "laranja";
-        magazineCarregador.tipoAlinhamento = "inline";
-        magazineCarregador.capacidade = 7;
-        magazineCarregador.qtdMunicao = 3;
+        System.out.println("Cor do transportador: ");
+        magazineCarregador.corTransportador = leia.next();
+        System.out.println("Tipo do carregador: ");
+        magazineCarregador.tipoAlinhamento = leia.next();
+        System.out.println("Capacidade: ");
+        magazineCarregador.capacidade = leia.nextInt();
+        System.out.println("Quantidade de munições atual: ");
+        magazineCarregador.qtdMunicao = leia.nextInt();
         return magazineCarregador;
     }
 
     public static void main(String[] args) {
+        Carregador newMag = cadastrarCarregador();
+        newMag.qtdMunicao = carregar(newMag.qtdMunicao, newMag.capacidade);
+        newMag.qtdMunicao = descarregar(newMag.qtdMunicao);
     }
 }
