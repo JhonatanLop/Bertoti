7254e167072590c8a39749a7d14d239dde0a5dd2
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-04-27 00:10:06 -0300

    atualização da classe loja

--- Engenharia de Software/Classes/Loja.java
+++ Engenharia de Software/Classes/Loja.java
@@ -21,37 +21,60 @@ public class Loja {
             case 1:
                 // lista armas
                 // for (int i = 0; i < Fabrica.getArsenalArmas().size(); i++) {
-                    //     int indice = i + 1;
-                    //     System.out.println(arsenalArmas.get(i) + "-" + indice + "\n");
-                    // }
-                    
+                //         int indice = i + 1;
+                //         System.out.println(Fabrica.getArsenalArmas(i) + "-" + indice + "\n");
+                //     }
+
+                int indice = 1;
                 System.out.println("Lista de Armas:");
                 for (Arma a : Fabrica.getArsenalArmas()) {
-                    System.out.println(a + "\n");
+                    System.out.println((indice + " - " ) + a + "\n");
+                    indice++;
                 }
 
                 // seleciona arma
                 System.out.println("Selecione uma arma:");
-                int arma = leia.nextInt();
+                int armaIndex = leia.nextInt();
+                Arma arma = Fabrica.getArsenalArmas().get(armaIndex - 1);
+                // compra arma
+                System.out.println("Gostaria de comprar a arma " + arma + "?");
+                System.out.println("1 - Sim");
+                System.out.println("2 - Nao");
+                // opcao
+                int opcao = leia.nextInt();
+                switch (opcao) {
+                    case 1:
+                        // compra arma
+                        // retira arma da lista
+                        // -- código
+                        System.out.println("Voce comprou a arma " + arma);
+                        break;
+                    case 2:
+                        // nao compra arma
+                        System.out.println("Voce nao comprou a arma " + arma);
+                        break;
+                    default:
+                        break;
+                }
                 break;
 
             case 2:
                 // lista carregadores
                 System.out.println("Lista de Balas:");
-                for (int i = 0; i < arsenalArmas.size(); i++) {
-                    int indice = 1;
-                    System.out.println(arsenalMag.get(i) + "-" + indice + "\n");
-                }
+                // for (int i = 0; i < arsenalArmas.size(); i++) {
+                //     int indice = 1;
+                //     System.out.println(arsenalMag.get(i) + "-" + indice + "\n");
+                // }
 
                 break;
 
             case 3:
                 // lista balas
                 System.out.println("Lista de balas:");
-                for (int i = 0; i < arsenalBuller.size(); i++) {
-                    int indice = 1;
-                    System.out.println(arsenalBuller.get(i) + "-" + indice + "\n");
-                }
+                // for (int i = 0; i < arsenalBuller.size(); i++) {
+                //     int indice = 1;
+                //     System.out.println(arsenalBuller.get(i) + "-" + indice + "\n");
+                // }
             default:
                 break;
         }
