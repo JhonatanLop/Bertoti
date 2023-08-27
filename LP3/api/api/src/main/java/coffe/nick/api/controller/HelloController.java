b62c62ff56bab71073fd28a3ac7c774155180ac1
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-08-27 14:46:11 -0300

    subindo projeto

---
+++ LP3/api/api/src/main/java/coffe/nick/api/controller/HelloController.java
@@ -0,0 +1,16 @@
+package coffe.nick.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Hello")
+public class HelloController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
+    }
+    
+}
