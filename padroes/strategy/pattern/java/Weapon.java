1dca4c0ad2bc7eae1b5466245609c1ac8872fbac
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-03-13 20:40:48 -0300

    Refactor Weapon class to use interface

--- padroes/strategy/pattern/java/Weapon.java
+++ padroes/strategy/pattern/java/Weapon.java
@@ -1,19 +1,7 @@
-public abstract class Weapon{
-    protected String name;
-    protected double damage;
-    protected int durability;
 
-    public void repair(int parts){
-        durability += parts;
-    }
 
-    public abstract double action();
-    public abstract double defeat(double damage);
+public interface Weapon {
 
-    public String getName() { return name; }
-    public double getDamage() { return damage; }
-    public int getDurability() { return durability; }
-    public void setName(String name) { this.name = name; }
-    public void setdamage(double damage) { this.damage = damage; }
-    public void setDurability(int durability) { this.durability = durability; }
+    public void repair(int parts);
+    public double use();
 }
