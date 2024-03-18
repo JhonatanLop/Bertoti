d6618e2b1520b3819d79a8602aec8ca67781eab3
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-03-17 23:11:51 -0300

    refatorando classe Player

--- padroes/strategy/pattern/java/Player.java
+++ padroes/strategy/pattern/java/Player.java
@@ -1,29 +1,35 @@
-
-
-import javax.naming.Context;
-
 public class Player {
     
     private String name;
     private int parts;
-    private Context context;
+    private Weapon weapon;
     
-    public Player(String name, int parts, Context context) {
+    public Player(String name, int parts, Weapon weapon) {
         this.name = name;
         this.parts = parts;
-        this.context = context;
+        this.weapon = weapon;
     }
-
-    public void setWeapon(Weapon weapon){
-        
+    
+    public double attack(){
+        return getWeapon().use();
     }
-
-    public void attack(){
-        context.executeStrategy();
-    }
-
+    
     public static void main(String[] args) {
-        Sword foo = new Sword();
+        Sword foo = new Sword("Darkrepulser",false,900,1100);
+        Player joe = new Player("jhonatan",50,foo);
+        double dmg = joe.attack();
+        System.out.println(dmg);
+        Shield shield = new Shield("snake",700,98000);
+        joe.setWeapon(shield);
+        double defeat = joe.attack();
+        System.out.println(defeat);
         
     }
+    
+    public String getName() { return name; }
+    public Weapon getWeapon() { return weapon; }
+    public int getParts() { return parts; }
+    public void setName(String name) { this.name = name; }
+    public void setWeapon(Weapon weapon) { this.weapon = weapon; }
+    public void setParts(int parts) { this.parts = parts; }
 }
