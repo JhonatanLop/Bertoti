76339563ff9bf3c742340931fe23e64934a1f4fa
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-04-08 20:31:15 -0300

    Add Component interface for composite pattern

---
+++ padroes/composite/pattern/java/Component.java
@@ -0,0 +1,9 @@
+package composite.pattern.java;
+
+public interface Component {
+    public void describe();
+    public String getName();
+    public String getDescription();
+    public double getDammage();
+}
+
