45d3310e35b0b437504223d0ba2a413b41d471aa
JhonatanLop <jhooliveira.lopes@gmail.com>
2024-04-15 21:08:11 -0300

    refactor: alter types and getInstance method

--- padroes/singleton/java/ConfigurationManager.java
+++ padroes/singleton/java/ConfigurationManager.java
@@ -5,32 +5,32 @@ import java.util.Date;
 public class ConfigurationManager {
     private static ConfigurationManager instance;
     private String name;
-    private Date date;
+    private String link;
     private int stock;
-    private String goal;
+    private int goal;
 
-    private ConfigurationManager(String name, Date date, int stock, String goal){
+    private ConfigurationManager(String name, String link, int stock, int goal){
         this.name = name;
-        this.date = date;
+        this.link = link;
         this.stock = stock;
         this.goal = goal;
     }
 
-    public static ConfigurationManager getInstance(String name, Date date, int stock, String goal){
+    public static ConfigurationManager getInstance(String name, String link, int stock, int goal){
         if (instance == null) {
-            instance = new ConfigurationManager("name form database", new Date(), 155, "teste");
+            instance = new ConfigurationManager(name, link, stock, goal);
         }
         return instance;
     }
 
     public static ConfigurationManager getInstance() { return instance; }
     public String getName() { return name; }
-    public Date getDate() { return date; }
+    public String getLink() { return link; }
     public int getStock() { return stock; }
-    public String getGoal() { return goal; }
+    public int getGoal() { return goal; }
     public static void setInstance(ConfigurationManager instance) { ConfigurationManager.instance = instance; }
     public void setName(String name) { this.name = name; }
-    public void setDate(Date date) { this.date = date; }
+    public void setLink(String link) { this.link = link; }
     public void setStock(int stock) { this.stock = stock; }
-    public void setGoal(String goal) { this.goal = goal; }
+    public void setGoal(int goal) { this.goal = goal; }
 }
